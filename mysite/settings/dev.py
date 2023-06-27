2116d4f82333fe2e6602c1239e4a5806fe517ce2
D.M. Oladele <damilolamohammed2@hotmail.com>
2023-06-27 13:32:49 +0100

    code for the Wagtail's 'Your first Wagtail site' with the BlogCategory model changed to Author

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-0qkhkguu*8q)*xp14rzj4h@$d=l_us*1s43pft7gt&md(ib+=q"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
